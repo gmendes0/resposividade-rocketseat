54319ff4b9e264f30aeb0b7d1a57912728ddf6ea
gmendes0 <gmendes230@gmail.com>
2020-03-19 23:50:00 -0300

    style: add responsive layout

--- inicial/scripts.js
+++ inicial/scripts.js
@@ -48,8 +48,7 @@ videos.map(video => {
   cardClone.setAttribute("id", video.video_id);
   cardClone.querySelector("img").src = video.thumb;
   cardClone.querySelector(".title").innerHTML = video.title;
-  cardClone.querySelector(".info > p.text--medium").innerHTML =
-    video.duration;
+  cardClone.querySelector(".info > p.text--medium").innerHTML = video.duration;
   sectionCards.appendChild(cardClone);
 });
 
